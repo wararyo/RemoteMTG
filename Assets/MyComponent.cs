258e5d0b17d43a0a5290bb5317aa5a28a5371e91
wararyo <wararyo@gmail.com>
2019-01-22 05:17:08 +0900

    Vuforiaのカメラ映像をTexture2DにしてJPG圧縮できるようになった

---
+++ Assets/MyComponent.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.UI;
+
+public class MyComponent : MonoBehaviour {
+
+    private bool mAccessCameraImage = true;
+    public UnityEngine.UI.Image UIImage;
+    Texture2D tex;
+
+    // The desired camera image pixel format
+    private Vuforia.Image.PIXEL_FORMAT mPixelFormat = Vuforia.Image.PIXEL_FORMAT.RGBA8888;// or RGBA8888, RGB888, RGB565, YUV
+    // Boolean flag telling whether the pixel format has been registered
+    private bool mFormatRegistered = false;
+    void Start()
+    {
+        // Register Vuforia life-cycle callbacks:
+        VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        VuforiaARController.Instance.RegisterOnPauseCallback(OnPause);
+        VuforiaARController.Instance.RegisterTrackablesUpdatedCallback(OnTrackablesUpdated);
+    }
+    /// <summary>
+    /// Called when Vuforia is started
+    /// </summary>
+    private void OnVuforiaStarted()
+    {
+        // Try register camera image format
+        if (CameraDevice.Instance.SetFrameFormat(mPixelFormat, true))
+        {
+            Debug.Log("Successfully registered pixel format " + mPixelFormat.ToString());
+            mFormatRegistered = true;
+        }
+        else
+        {
+            Debug.LogError("Failed to register pixel format " + mPixelFormat.ToString() +
+                "\n the format may be unsupported by your device;" +
+                "\n consider using a different pixel format.");
+            mFormatRegistered = false;
+        }
+    }
+    /// <summary>
+    /// Called when app is paused / resumed
+    /// </summary>
+    private void OnPause(bool paused)
+    {
+        if (paused)
+        {
+            Debug.Log("App was paused");
+            UnregisterFormat();
+        }
+        else
+        {
+            Debug.Log("App was resumed");
+            RegisterFormat();
+        }
+    }
+    /// <summary>
+    /// Called each time the Vuforia state is updated
+    /// </summary>
+    private void OnTrackablesUpdated()
+    {
+        if (mFormatRegistered)
+        {
+            if (mAccessCameraImage)
+            {
+                Vuforia.Image image = CameraDevice.Instance.GetCameraImage(mPixelFormat);
+                if (image != null)
+                {
+                    string imageInfo = mPixelFormat + " image: \n";
+                    imageInfo += " size: " + image.Width + " x " + image.Height + "\n";
+                    imageInfo += " bufferSize: " + image.BufferWidth + " x " + image.BufferHeight + "\n";
+                    imageInfo += " stride: " + image.Stride;
+                    Debug.Log(imageInfo);
+                    tex = new Texture2D(image.Width, image.Height, TextureFormat.RGBA32, false);
+                    tex.filterMode = FilterMode.Point;
+                    image.CopyToTexture(tex);
+                    Debug.Log("JPG :" + tex.EncodeToJPG(50).Length);
+                    tex.Apply();
+                    Color[] pixels = tex.GetPixels();//image.Pixels;
+                    UIImage.sprite = Sprite.Create(tex, new Rect(0, 0, image.Width, image.Height), Vector2.zero);
+                    if (pixels != null && pixels.Length > 0)
+                    {
+                        Debug.Log("Image pixels: " + pixels[0] + "," + pixels[1] + "," + pixels[2] + ",...");
+                    }
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// Unregister the camera pixel format (e.g. call this when app is paused)
+    /// </summary>
+    private void UnregisterFormat()
+    {
+        Debug.Log("Unregistering camera pixel format " + mPixelFormat.ToString());
+        CameraDevice.Instance.SetFrameFormat(mPixelFormat, false);
+        mFormatRegistered = false;
+    }
+    /// <summary>
+    /// Register the camera pixel format
+    /// </summary>
+    private void RegisterFormat()
+    {
+        if (CameraDevice.Instance.SetFrameFormat(mPixelFormat, true))
+        {
+            Debug.Log("Successfully registered camera pixel format " + mPixelFormat.ToString());
+            mFormatRegistered = true;
+        }
+        else
+        {
+            Debug.LogError("Failed to register camera pixel format " + mPixelFormat.ToString());
+            mFormatRegistered = false;
+        }
+    }
+}
